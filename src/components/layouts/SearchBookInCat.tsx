271fdeae10387edb763698074ccd4db4c1e9b20c
shafek <engahmedshafek1@gmail.com>
2024-09-04 15:24:53 +0300

    up

--- src/components/layouts/SearchBookInCat.tsx
+++ src/components/layouts/SearchBookInCat.tsx
@@ -3,7 +3,11 @@ import { useParams } from "react-router-dom";
 const SearchBookInCat = () => {
   const { name } = useParams();
   console.log(name);
-  return <div></div>;
+  return (
+    <div className=" text-center text-custom-teal font-semibold mt-14">
+      This page is under construction. Please check back later.
+    </div>
+  );
 };
 
 export default SearchBookInCat;
